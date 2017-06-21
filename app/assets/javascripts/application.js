47f2cd7912c09c29fd03daf24a47bed8bf20e52b
tnepper <Talia@MacBook-2.local>
2017-06-21 18:10:09 -0500

    Created lightbox for portfolio page

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,4 +1,4 @@
-// app/assets/javascripts/wedding.js
+
 
 // This is a manifest file that'll be compiled into application.js, which will include all the files
 // listed below.
@@ -15,8 +15,45 @@
 //= require jquery
 //= require bootstrap-sprockets
 //= require jquery_ujs
-//= require photoswipe
 //= require turbolinks
 //= require_tree .
 
 $('.carousel').carousel();
+
+//js for lightboxes
+function openModal() {
+  document.getElementById('myModal').style.display = "block";
+}
+
+function closeModal() {
+  document.getElementById('myModal').style.display = "none";
+}
+
+var slideIndex = 1;
+showSlides(slideIndex);
+
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("box-mySlides");
+  var dots = document.getElementsByClassName("box-demo");
+  var captionText = document.getElementById("caption");
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";
+  dots[slideIndex-1].className += " active";
+  captionText.innerHTML = dots[slideIndex-1].alt;
+}
