0b729992f47271f4756c9254a089a411b17a20e6
tnepper <Talia@MacBook-2.local>
2017-06-30 12:17:59 -0500

    Working to incorporate multiple photo uploads to photo uploader

--- app/controllers/photo_uploads_controller.rb
+++ app/controllers/photo_uploads_controller.rb
@@ -69,6 +69,6 @@ class PhotoUploadsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def photo_upload_params
-      params.require(:photo_upload).permit(:image)
+      params.require(:photo_upload).permit({images: []})
     end
 end
